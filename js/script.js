2475ab57f60c796fc37c775ca339cdf157353eec
Nico <nicoisaia@outlook.com>
2023-02-23 18:04:52 -0300

    added basic mathematical operations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
 function add(a, b) {
     return a + b;
-}
+};
+
+function substract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    if (b === 0) return "error";
+    return a/b;
+};
+
+function operate(a, b, op) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return substract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return "incorrect operator";
+    }
+};
+
