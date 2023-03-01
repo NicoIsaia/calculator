ddf72bc558731c0cf751e4428ede5016da4be166
Nico <nicoisaia@outlook.com>
2023-03-01 18:57:14 -0300

    Numbers clicked, touched or keyed are displayed

--- js/script.js
+++ js/script.js
@@ -30,3 +30,37 @@ function operate(a, b, op) {
     }
 };
 
+let displayVar = "";
+
+const numbers = document.querySelectorAll("button.number");
+let display = document.querySelector("#display-value");
+
+function numbLogger(event) {
+    let num = event.target;
+    if (displayVar.length < 15) {
+        if (num.textContent === "." && displayVar.includes(".")) {
+            // do nothing
+        } else {
+            displayVar += num.textContent;
+            display.textContent = displayVar;
+        }
+    }
+}
+
+function keyLogger(event) {
+    let num = event.key;
+    if (displayVar.length < 15) {
+        if (num === "." && displayVar.includes(".")) {
+            // do nothing
+        } else {
+            displayVar += num;
+            display.textContent = displayVar;
+        }
+    }
+}
+
+document.addEventListener("keydown", keyLogger);
+
+numbers.forEach((number) => number.addEventListener("click", numbLogger));
+numbers.forEach((number) => number.addEventListener("touchstart", numbLogger));
+
