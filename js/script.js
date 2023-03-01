9e16e4f3742d55bf9ec64995e0192659ae2e45f1
Nico <nicoisaia@outlook.com>
2023-03-01 19:09:03 -0300

    Filtering allowed keypresses but I have to go

--- js/script.js
+++ js/script.js
@@ -49,6 +49,10 @@ function numbLogger(event) {
 
 function keyLogger(event) {
     let num = event.key;
+    const allowedNums = ["0", "1", "2", "3", "4"] // QUEDASTE ACÁ!!
+    /* Completar allowed con las teclas permitidas.
+        para después usar allowedNums.includes(num) para comprobar si es
+        una de las teclas permitidas */
     if (displayVar.length < 15) {
         if (num === "." && displayVar.includes(".")) {
             // do nothing
@@ -63,4 +67,10 @@ document.addEventListener("keydown", keyLogger);
 
 numbers.forEach((number) => number.addEventListener("click", numbLogger));
 numbers.forEach((number) => number.addEventListener("touchstart", numbLogger));
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    displayVar = "";
+    display.textContent = "0";
+})
 
