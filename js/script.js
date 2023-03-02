0c11578188f03d0f715ee691895c0620fce82ccb
Nico <nicoisaia@outlook.com>
2023-03-02 18:17:49 -0300

    Calculator beta

--- js/script.js
+++ js/script.js
@@ -30,6 +30,8 @@ function operate(a, b, op) {
     }
 };
 
+let oper = "";
+let firstNum = "";
 let displayVar = "";
 
 const numbers = document.querySelectorAll("button.number");
@@ -49,28 +51,101 @@ function numbLogger(event) {
 
 function keyLogger(event) {
     let num = event.key;
-    const allowedNums = ["0", "1", "2", "3", "4"] // QUEDASTE ACÁ!!
-    /* Completar allowed con las teclas permitidas.
-        para después usar allowedNums.includes(num) para comprobar si es
-        una de las teclas permitidas */
-    if (displayVar.length < 15) {
-        if (num === "." && displayVar.includes(".")) {
-            // do nothing
-        } else {
-            displayVar += num;
-            display.textContent = displayVar;
+    const allowedNums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."] 
+    if (allowedNums.includes(num)) {
+        if (displayVar.length < 15) {
+            if (num === "." && displayVar.includes(".")) {
+                // do nothing
+            } else {
+                displayVar += num;
+                display.textContent = displayVar;
+            }
         }
+    }
+    if (num === "c" || num === "C") {
+        clearMem();
+    }
+    const symbols = ["+", "-", "*", "/", "Enter"]
+    if (symbols.includes(num)) {
+        if (num === "Enter") {
+            if (oper) {
+                if (!displayVar) displayVar = 0;
+                if (!firstNum) firstNum = 0;
+                if (!oper) oper = "+";
+                displayVar = operate(parseFloat(firstNum), parseFloat(displayVar), oper);
+                display.textContent = Math.round((displayVar + Number.EPSILON) * 100) / 100;
+                firstNum = "";
+            }
+        } else {
+            operationWithKeys(num);
+        }
+    }
+}
+
+function clearMem() {
+    firstNum = "";
+    displayVar = "";
+    display.textContent = "0";
+}
+
+function operation(event) {
+    if (!firstNum) {
+        if (!displayVar) displayVar = 0;
+        firstNum = displayVar;
+        oper = event.target.textContent;
+        displayVar = "";
+    } else {
+        if (!displayVar) displayVar = 0;
+        displayVar = operate(parseFloat(firstNum), parseFloat(displayVar), oper);
+        oper = event.target.textContent;
+        display.textContent = Math.round((displayVar + Number.EPSILON) * 100) / 100;
+        firstNum = displayVar;
+        displayVar = "";
+    }
+}
+
+function operationWithKeys(key) {
+    if (!firstNum) {
+        if (!displayVar) displayVar = 0;
+        firstNum = displayVar;
+        oper = key;
+        displayVar = "";
+    } else {
+        if (!displayVar) displayVar = 0;
+        displayVar = operate(parseFloat(firstNum), parseFloat(displayVar), oper);
+        oper = key;
+        display.textContent = Math.round((displayVar + Number.EPSILON) * 100) / 100;
+        firstNum = displayVar;
+        displayVar = "";
     }
 }
 
 document.addEventListener("keydown", keyLogger);
 
 numbers.forEach((number) => number.addEventListener("click", numbLogger));
-numbers.forEach((number) => number.addEventListener("touchstart", numbLogger));
+//numbers.forEach((number) => number.addEventListener("touchstart", numbLogger));
 
 const clear = document.querySelector('.clear');
-clear.addEventListener('click', () => {
-    displayVar = "";
-    display.textContent = "0";
-})
+clear.addEventListener('click', clearMem);
 
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const multiplicate = document.querySelector('.multiplicate');
+const divi = document.querySelector('.divide');
+
+plus.addEventListener('click', operation);
+minus.addEventListener('click', operation);
+multiplicate.addEventListener('click', operation);
+divi.addEventListener('click', operation);
+
+const equal = document.querySelector('.equal');
+equal.addEventListener('click', () => {
+    if (oper) {
+        if (!displayVar) displayVar = 0;
+        if (!firstNum) firstNum = 0;
+        if (!oper) oper = "+";
+        displayVar = operate(parseFloat(firstNum), parseFloat(displayVar), oper);
+        display.textContent = Math.round((displayVar + Number.EPSILON) * 100) / 100;
+        firstNum = "";
+    } 
+});
